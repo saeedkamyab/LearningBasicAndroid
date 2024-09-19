d0efb76a936706da166eb28c5d3ba2f8204e6e79
saeedkamyab <saeed.kamyab@yahoo.com>
2024-09-19 10:26:34 +0330

    init

---
+++ app/src/main/java/com/example/learningandroidfrombase/Season_02.kt
@@ -0,0 +1,35 @@
+package com.example.learningandroidfrombase
+
+fun main(){
+
+
+
+
+
+
+
+    //Lambda and Extension Methods
+    val sum={
+            x:Double,y:Double->x+y
+    }
+    val a:Double=3.0
+    a.isPrime();
+    divide(10)
+    println(sum(30.0,20.0))
+//---------------------------------------------
+}
+
+
+
+
+//Lambda and Extension Methods
+fun divide(number:Int)=number%2==0;
+fun Double.isPrime():Boolean{
+    for(i in 2 until this .toInt()){
+        if(this.toInt()%i==0){
+            return  false
+        }
+    }
+    return true;
+}
+//---------------------------------------------
